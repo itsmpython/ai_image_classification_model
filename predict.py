a47078ae25d3d400c53e3b9ecc90f7ad9d632c4c
Mallikarjun Somanakatti <mallikarjun.somanakatti@gmail.com>
2018-08-23 13:32:58 -0700

    Finetuning argparse and few other enhancements

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@
 #                                                                             
 # PROGRAMMER: Mallikarjun Somanakatti
 # DATE CREATED: 07/10/2018                                  
-# REVISED DATE: 07/14/2018 
+# REVISED DATE: 07/25/2018 
 # PURPOSE:  This program predict the class for an input image by useing a pre-trained model
 #           Model is trained using train.py which uses vgg16 model with its classifier reprogrammed.
 #
@@ -22,7 +22,7 @@ import pandas as pd
 from torch import nn 
 from torch import optim 
 from torchvision import datasets, transforms, models 
-import helper
+import load_pretrained_model as pm
 
 def main():
     # Measures total program runtime by collecting start time
@@ -32,14 +32,16 @@ def main():
     in_arg = get_input_args()
     
     # Load and rebuild a model
-    mymodel = load_checkpoint(in_arg.cp_path, in_arg.checkpoint)
+    mymodel = load_checkpoint(in_arg.cp_path, in_arg.checkpoint, in_arg.arch)
     
     # Process Image before feeding into the model
     input_image = process_image(in_arg.image_path)
     
     # The script successfully reads in an image and a checkpoint 
     # then prints the most likely image class and it's associated probability
-    predict(in_arg.image_path, mymodel, in_arg.topK)
+    # predict(image_path, model, class_to_idx, topk):
+    #predict(in_arg.image_path, mymodel[0], mymodel[1], in_arg.topK, in_arg.gpu)
+    predict(input_image, mymodel[0], mymodel[1], in_arg.topK, in_arg.gpu)
 
 # Functions defined below   
 def get_input_args():
@@ -47,65 +49,82 @@ def get_input_args():
     parser = argparse.ArgumentParser(description='Predicts the class for an input image')
     parser.add_argument('-i', '--image_path', metavar='', help ='Image file path')
     parser.add_argument('-p', '--cp_path', metavar='', help ='Checkpoint path')
+    parser.add_argument('-a', '--arch', metavar='', help ='Neural Network Architecture(coded for ''vgg16'' and ''alexnet'' for now)')      
     parser.add_argument('-c', '--checkpoint', metavar='', help ='Checkpoint Name')
     parser.add_argument('-K', '--topK', type = int,  metavar = '', help ='List top K classes predicted')
+    parser.add_argument('-g', '--gpu', metavar='', help ='Pass ''Y'' or ''N'' for gpu')
         
     return parser.parse_args()
 
-def load_checkpoint(filepath, checkpoint_name):
-    # Loads a checkpoint and rebuilds the model
+def load_checkpoint(filepath, checkpoint_name, arch):
+    # Loads a checkpoint and rebuilds the model    
     checkpoint = torch.load(filepath + checkpoint_name) # load the .pth file
-    mymodel = checkpoint['model'] # get the model part from the checkpoint
-    mymodel.load_state_dict(checkpoint['state_dict']) #get the state_dict
+    mymodel = pm.load_pretrained_model(arch, checkpoint['hidden_layers'])
+    mymodel_class_to_idx = checkpoint['class_to_idx']
+    mymodel_state_dict = checkpoint['state_dict']
     
-    #print('VGG16 Model Classifier :--> \n', mymodel.classifier)
-    #print('Model class_to_idx :--> \n', mymodel.class_to_idx)
-    print('Model Checkpoint Loaded')
-    return mymodel
+    print('\n Model Checkpoint Loaded \n')
+    return mymodel, mymodel_class_to_idx, mymodel_state_dict
 
 def process_image(image_path):
     ''' Scales, crops, and normalizes a PIL image for a PyTorch model, returns an Numpy array.
     This is necessary becasue the image used for predcition should normalized similar to the ones used 
     for training the model
     '''
+    # TODO: Process a PIL image for use in a PyTorch model
     im = Image.open(image_path)
-        
-    #RESIZE IMAGE
-    im.resize((256, 256))    
-    #CROP IMAGE
-    width, height = im.size   # Get dimensions
-    new_width = 224
-    new_height = 224
-
-    left = (width - new_width)/2
-    top = (height - new_height)/2
-    right = (width + new_width)/2
-    bottom = (height + new_height)/2
-
-    im=im.crop((left, top, right, bottom))
-    print('Image Size is : ', im.size)    
+    print('Original Image size : ', im.size)
+    width, height = im.size
+    size = 256, 256
     
+    #RESIZE IMAGE
+    if width > height:
+        ratio = float(width)/float(height)
+        newheight = ratio * size[0]
+        height = newheight
+        im = im.resize(((size[0], int(newheight))), Image.ANTIALIAS)
+    else:
+        ratio - float(height) / float(width)
+        newwidth = ratio * size[0]
+        width = newwidth
+        im = Image.resize(((int(newwidth), size[0])), Image.ANTIALIAS)
+        
+    width, height = im.size   # Get dimensions
+    
+    left = (width - 224)/2
+    top = (height - 224)/2
+    right = (width + 224)/2
+    bottom = (height + 224)/2
+
+    cropped_im = im.crop((left, top, right, bottom))
+    
+    ## COMMENTED another method that I could use
+    #new_width, new_height = width, height
+    #cropped_im = im.crop((new_width/2-112, new_height/2-112, new_width/2+112, new_height/2+112))    
+    ## End of COMMENTING
+
     #Color channels of images are typically encoded as integers 0-255, but the model expected floats 0-1
     # Conver to Numpy array
-    np_image = np.array(im)
+    np_image=np.array(cropped_im)/255
+
     means = np.array([0.485, 0.456, 0.406])
     stds = np.array([0.229, 0.224, 0.225])
-    
-    #NORMALIZE
-    im = (np_image/255 - means)/stds
-    
-    # TRANSPOSE IMAGE
-    # And finally, PyTorch expects the color channel to be the first dimension but it's the third dimension in the PIL image and Numpy array. You can reorder dimensions using ndarray.transpose. The color channel needs to be first and retain the order of the other two dimensions.
-    img_tp = im.transpose(2, 0, 1)   
-    #print('Printing Processed Image :--> \n', img_tp)
-    print('Input Image processing completed')
-    return img_tp
 
-def predict(image_path, model, topk):
+    #NORMALIZE
+    normalized_image = (np_image - means)/stds
+    # TRANSPOSE IMAGE
+    final_image =normalized_image.transpose((2, 0, 1))
+    print('Final Image shape', final_image.shape)
+        
+    return final_image
+
+def predict(image_path, model, class_to_idx, topk, gpu):
     # Code to predict the class from an image file
     ''' Predict the class (or classes) of an image using a trained deep learning model.
     '''    
-    img = process_image(image_path)    
+    #img = process_image(image_path)
+    img = image_path
+    model_class_to_idx = class_to_idx
     
     #Load the model
     #model = load_checkpoint(model)
@@ -116,49 +135,54 @@ def predict(image_path, model, topk):
     np_image = np.array(img)
     imgT= torch.from_numpy(np_image)    #From Numpy to torch Tensor
 
-    is_cuda = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if gpu == 'Y':
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        print("Device : ", device)
+    else:
+        device = "cpu"
+        print("Device : ", device)
+    
+    imgT = imgT.to(device)
+    model.to(device)
+    
+    imgS = imgT.unsqueeze_(0).float()
 
-    if is_cuda:
-        imgT = imgT.cuda()
-        model.cuda()
-    
-        imgS = imgT.unsqueeze_(0).float()
-    
-        # Run image through model    
-        output = model.forward(imgS)  
-        ps = torch.exp(output)        
-        
-        probs, indices = ps.topk(topk)        
-        
-        # Inorder to access the array from indices (which is a tensor)
-        # use .numpy(). We use .cpu() to run it through cpu
-        indices = indices.cpu().numpy()[0]
-                
-        # invert class_to_idx dict        
-        idx_to_class = {i:c for c,i in model.class_to_idx.items()}
-        classes = {idx_to_class[i] for i in indices}
-        
-        # Load the json file with flowernames and its class keys
-        import json        
-        with open('cat_to_name.json', 'r') as f:
-            cat_to_name = json.load(f)
+    # Run image through model    
+    output = model.forward(imgS)  
+    ps = torch.exp(output)        
+
+    probs, indices = ps.topk(topk)        
+
+    # Inorder to access the array from indices (which is a tensor)
+    # use .numpy(). We use .cpu() to run it through cpu
+    indices = indices.cpu().numpy()[0]
+
+    # invert class_to_idx dict        
+    #idx_to_class = {i:c for c,i in model.class_to_idx.items()}
+    idx_to_class = {i:c for c,i in model_class_to_idx.items()}
+    classes = {idx_to_class[i] for i in indices}
+
+    # Load the json file with flowernames and its class keys
+    import json        
+    with open('cat_to_name.json', 'r') as f:
+        cat_to_name = json.load(f)
 
         # Iterate through the classes returned by the model and find its corresponding flower_name
         flower_name = []
         for id in classes:
             flower_name.append(cat_to_name[id].capitalize())
-            
-        # Create a Pandas Series to be used with plotting
-        # Create DataFrame from the output data to be used in plotting
-        prob_predictions = pd.Series(data=probs.tolist(), index=probs.tolist())
-        
-        data = {'Flower_Names' : pd.Series(data=flower_name),
-                'Probabilities': pd.Series(data=probs.data[0])
-               }
-        
+
+            # Create a Pandas Series to be used with plotting
+            # Create DataFrame from the output data to be used in plotting
+            prob_predictions = pd.Series(data=probs.tolist(), index=probs.tolist())
+
+            data = {'Flower_Names' : pd.Series(data=flower_name),
+                    'Probabilities': pd.Series(data=probs.data[0])
+                   }
+
         df = pd.DataFrame(data)
         print(df);
     
     return probs, classes, flower_name, prob_predictions, df
 if __name__ == '__main__':    
-    main()   
+    main()
--- train.py
+++ train.py
@@ -4,7 +4,7 @@
 #                                                                             
 # PROGRAMMER: Mallikarjun Somanakatti
 # DATE CREATED: 07/06/2018                                  
-# REVISED DATE: 07/14/2018 
+# REVISED DATE: 07/23/2018 
 # PURPOSE: Image Classifier Project for the AIPND Program
 #           Check flower images & report results: read them in, predict their
 #           class, compare prediction to actual value labels from the cat_to_name.json file
@@ -40,17 +40,16 @@ def main():
     data = loads_transforms_data(in_arg.data_dir)
     
     # Loads a pre-trained model and updates is classifier to define out model
-    model = load_pretrained_model(in_arg.arch)
+    model = load_pretrained_model(in_arg.arch, in_arg.hiddenunits)
     
     # Trains the model and prints running and training loss
-    trained_model = train_model(model, data[3], in_arg.learn_rate, in_arg.epochs, in_arg.gpu)
+    trained_model = train_model(model, data[3], data[4], in_arg.learn_rate, in_arg.epochs, in_arg.gpu)
     
     # Tests the model for test images (From test data set) and prints accurcy
     test_network_model(trained_model[0], data[5])
     
-    # Save the Model Checkpoint
-    #save_model_checkpoint(model, epochs, optimizer, train_datasets):
-    save_model_checkpoint(trained_model[0], in_arg.arch, in_arg.epochs, trained_model[2], data[0])    
+    # Save the Model Checkpoint    
+    save_model_checkpoint(trained_model[0], in_arg.arch, in_arg.epochs, in_arg.learn_rate, trained_model[2], in_arg.hiddenunits, data[0]) 
     
     # Measure total program runtime by collecting end time
     end_time = time()
@@ -67,7 +66,8 @@ def get_input_args():
     parser = argparse.ArgumentParser(description='Trains a Model and saves the Checkpoint')
     parser.add_argument('-d', '--data_dir', metavar='', help ='Image Data Directory to train, validate and test')
     parser.add_argument('-a', '--arch', metavar='', help ='Neural Network Architecture(coded for ''vgg16'' and ''alexnet'' for now)')
-    parser.add_argument('-l', '--learn_rate', type = float,  metavar = '', help ='Learning Rate set for training the model')
+    parser.add_argument('-u', '--hiddenunits', metavar='', type = int, nargs='+', help ='Number of hidden Units')
+    parser.add_argument('-l', '--learn_rate', type = float, metavar = '', help ='Learning Rate set for training the model')
     parser.add_argument('-e', '--epochs', metavar='', type = int, help ='Number of Epochs')
     parser.add_argument('-g', '--gpu', metavar='', help ='Pass ''Y'' or ''N'' for gpu')
     
@@ -87,31 +87,33 @@ def loads_transforms_data(data_dir):
     #return data[0], data[1], data[3], data[4]
     return train_datasets, valid_datasets, test_datasets, trainloader, validloader, testloader    
 
-def load_pretrained_model(arch):
+def load_pretrained_model(arch, hiddenunits):
     # Invoke a function "load_oretrained_model" which loads a pretrained vgg16model and
     # replaces its classifier according to the data we like to classify
-    mymodel = pm.load_pretrained_model(arch)
+    mymodel = pm.load_pretrained_model(arch, hiddenunits)
     return mymodel
 
-def train_model(model, train_data, learn_rate, nepochs, gpu):
+def train_model(model, train_data, validation_data, learn_rate, nepochs, gpu):
     # Define HYPER Parameters    
-    lr = learn_rate
+    learn_rate = learn_rate
     epochs = nepochs
     training_loss = 0
     running_loss = 0
     test_loss = 0     
     print_every = 40
     steps = 0
+    accuracy = 0
+    validation_loss = 0
     
-    # Define Loss Criteria and the Optimizer
     #myvgg16model = load_pretrained_model()    
     modeltotrain = model
-    print(modeltotrain.classifier)
-    
+    print('train.py modeltotrain.classifier = \n', modeltotrain.classifier)
+
+    # Define Loss Criteria and the Optimizer
     criterion = nn.NLLLoss()
-    optimizer = optim.Adam(modeltotrain.classifier.parameters(), lr = lr)
-    print(lr)
-    print(optimizer)
+    optimizer = optim.Adam(modeltotrain.classifier.parameters(), lr = learn_rate)
+    print(learn_rate)
+    print('Printing Optimizer', optimizer)
     
     # Train the classifier layers using backpropagation using the pre-trained network to get the features
     if gpu == 'Y':
@@ -139,6 +141,7 @@ def train_model(model, train_data, learn_rate, nepochs, gpu):
         Because of this we do not need to use "for images, labels in iter(trainloader)" '''     
 
         trainloader = train_data
+        validloader = validation_data
 
         for ii, (inputs, labels) in enumerate(trainloader):
             steps += 1
@@ -158,10 +161,24 @@ def train_model(model, train_data, learn_rate, nepochs, gpu):
             
             if steps % print_every == 0:
                 modeltotrain.eval()
+                
+                for ii, (inputs, labels) in enumerate(validloader):
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    
+                    predicted_outputs = modeltotrain.forward(inputs)
+                    validation_loss = criterion(predicted_outputs, labels).item()
+                    # Calculating the accuracy
+                    ps = torch.exp(predicted_outputs).data
+                    
+                    # Class with the highest probability is our predicted class
+                    equality = (labels.data == ps.max(dim=1)[1])
+                    accuracy = equality.type(torch.FloatTensor).mean()
+                        
                 print("Epoch : {}/{}...".format(e+1, epochs),
                       "Training Loss :{:4f}".format(running_loss/print_every),
-                      "Validation Loss: {:.3f}.. ".format(test_loss/len(trainloader)))
-
+                      "Validation Loss :{:3f}".format(validation_loss),
+                      "Validation Accuracy :{:3f}".format(accuracy))
+            
                 running_loss = 0
                 modeltotrain.train()
     return modeltotrain, criterion, optimizer
@@ -187,38 +204,47 @@ def test_network_model(model, test_data):
 
             # Accuracy is number of correct predictions divided by all predictions, just take the mean
             accuracy += (predicted == labels).sum().item()            
-        print('Accuracy of the network on the test images: %d %%' % (100 * accuracy / total))    
+        print('\n\n Validation Accuracy of the network on the test images: %d %%' % (100 * accuracy / total))    
         return accuracy
 
-def save_model_checkpoint(model, arch, epochs, optimizer, train_datasets):
+def save_model_checkpoint(model, arch, epochs, learn_rate, optimizer, hidden_units, train_datasets):
     
-    # Following is ond of the methogs to save without saving other details of the model
-    # torch.save(vgg16model.state_dict(), 'vgg16model_checkpoint.pth')    
+    # Following is ond of the methogs to save without saving other details of the model    
+    
+    if arch == 'vgg16':
+        input_size = 25088
+    elif arch == 'alexnet':
+        input_size = 9216
+        
+    hidden_sizes = hidden_units
+    output_size = 102
     
     # Saving class_to_index
+    #vgg16model.class_to_idx = train_datasets.class_to_idx
     model.class_to_idx = train_datasets.class_to_idx
 
     ## We probably want to save other things such as the mapping of classes to indices which you get from one of the image datasets
     ## image_datasets['train'].class_to_idx. 
     ## You can attach this to the model as an attribute which makes inference easier later on.        
-    checkpoint = {
-        'model': model,
-        'epochs': epochs,
-        'batch_size': 32, 
-        'classifier': model.classifier,
-        'optimizer': optimizer.state_dict(),
-        'state_dict': model.state_dict(),
-        'class_to_idx': model.class_to_idx,
-        'criterion': nn.NLLLoss()
-        } 
+    checkpoint = {'input_size': input_size,
+                  'output_size': output_size,
+                  'hidden_layers': hidden_sizes,
+                  'batch_size': 32,
+                  'learning_rate': learn_rate,
+                  'model_name': model,
+                  'classifier': model.classifier,
+                  'state_dict': model.state_dict(),
+                  'optimizer': optimizer.state_dict(),
+                  'epoch': epochs,
+                  'class_to_idx': model.class_to_idx} 
     
     if arch == 'vgg16':
         #torch.save(checkpoint, 'vgg16checkpoint.pth', '/home/workspace/aipnd-project') 
         torch.save(checkpoint, 'checkpoint.pth') 
-        print('VGG16 Model Checkpoint saved')
+        print('\n VGG16 Model Checkpoint saved')
     elif arch == 'alexnet':
         torch.save(checkpoint, 'alexnetcheckpoint.pth') 
-        print('Alexnet Checkpoint saved')
+        print('\n Alexnet Checkpoint saved')
     
 if __name__ == '__main__':    
     main()
